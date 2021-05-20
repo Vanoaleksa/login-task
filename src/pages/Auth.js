8e03178a6ed165d937d76460418eb8d1f1cae4a8
Vanoaleksa <vanoaleksa111@yandex.by>
2021-05-20 20:56:13 +0300

    Adding "useEffect" and axios

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,16 +1,32 @@
 import './Auth.module.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 
 const Auth = () => {
-    let a = document.querySelector('username');
-    console.log(a);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit=(e) =>{
+        e.preventDefault();
+    }
+
+    useEffect(() => {
+        console.log('useeffect')
+        axios('https://my.api.org/Account/Login',
+        )
+    );
+
+    console.log(email, password)
     return (
-        <div className='login'>
-            <label>Login</label>
-            <input  type='text' name='username'  />
-            <label>Password</label>
-            <input type='text' name='password'  />
-            <button type="submit">Sign In</button>
+        <div>
+            <form onSubmit={onSubmit}>
+                <label>Email</label>
+                <input type='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label>Password</label>
+                <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
