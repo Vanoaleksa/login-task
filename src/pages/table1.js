201b85d473b8ea2f901499ab542a96e94294d8f4
Vanoaleksa <vanoaleksa111@yandex.by>
2021-06-01 14:51:09 +0300

    Add function onClick and change state

--- src/pages/table1.js
+++ src/pages/table1.js
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../css/Table.css";
 import TableItem from "./TableItem";
 
-let data = [
+let dataArray = [
   { country: "Belarus", age: "21", sex: "men" },
   { country: "Portugal", age: "44", sex: "men" },
   { country: "China", age: "33", sex: "men" },
 ];
 
-
 const Table1 = () => {
+  const [data, setData] = useState(dataArray);
+  console.log(data);
+
+  const addNewItem = () => {
+    setData((oldArray) => [...oldArray, { country: "", age: "", sex: "" }]);
+  };
+
   return (
     <div>
       <div className="Header">
@@ -19,15 +25,14 @@ const Table1 = () => {
       </div>
       <div className="Table_container">
         <div className="Table_element">
-          
           {data.map((el) => (
-            <TableItem  country={el.country} age={el.age} sex={el.sex} />
+            <TableItem country={el.country} age={el.age} sex={el.sex} />
           ))}
         </div>
       </div>
       <div className="Buttons">
         <div className="NewButton">
-          <button>New</button>
+          <button onClick={addNewItem}>New</button>
         </div>
         <div className="DeleteButton">
           <button>Delete</button>
