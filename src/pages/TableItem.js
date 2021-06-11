de302bc5bf18aacad6a5d2bb15cda03cca31c689
Vanoaleksa <vanoaleksa111@yandex.by>
2021-06-11 15:17:31 +0300

    cancelButton

--- src/pages/TableItem.js
+++ src/pages/TableItem.js
@@ -3,33 +3,39 @@ import "../css/Table.css";
 
 const TableItem = (props) => {
   const countryField = useRef(null);
-  console.log(countryField)
   const sexField = useRef(null);
-  console.log(sexField)
   const ageField = useRef(null);
-  
+
   const [isStatus, setIsStatus] = useState(false);
   const editeItem = () => {
     setIsStatus(!isStatus);
   };
   const saveItem = () => {
-    console.log('he')
     setIsStatus(!isStatus);
-    console.log('sadasdasde')
     props.setData((lastArray) => [
       ...lastArray.map((data) => {
         if (props.id === data.id) {
           return {
-            id:props.id, country: countryField.current.value, age: ageField.current.value, sex: sexField.current.value
-          }
+            id: props.id,
+            country: countryField.current.value,
+            age: ageField.current.value,
+            sex: sexField.current.value,
+          };
+        } else {
+          return data;
         }
-        else {
-          return data
-        }
-      })])
+      }),
+    ]);
   };
 
-  
+  const cancelItem = () => {
+    setIsStatus(!isStatus);
+    props.setData((lastArray) => [
+      ...lastArray.map((data) => {
+        return data;
+      }),
+    ]);}
+    
 
   return (
     <div className="row">
@@ -45,34 +51,38 @@ const TableItem = (props) => {
       </label>
       <div className="rowItem">
         {isStatus ? (
-          <input  defaultValue={props.country} ref={countryField}></input>
+          <input defaultValue={props.country} ref={countryField}></input>
         ) : (
-          props.country
+        props.country
         )}
       </div>
       <div className="rowItem">
         {isStatus ? (
-          <input  defaultValue={props.age} ref={ageField}></input>
+          <input defaultValue={props.age} ref={ageField}></input>
         ) : (
           props.age
         )}
       </div>
       <div className="rowItem">
         {isStatus ? (
-          <input  defaultValue={props.sex} ref={sexField}></input>
+          <input defaultValue={props.sex} ref={sexField}></input>
         ) : (
           props.sex
         )}
       </div>
       <div className="editeButton">
         {isStatus ? (
-          <button onClick={saveItem}>Save</button>
+          <button className="buttonSave" onClick={saveItem}>
+            {" "}
+            Save{" "}
+          </button>
         ) : (
-          <button id={props.id} onClick={editeItem}>
+          <button className="buttonEdite" onClick={editeItem}>
             {" "}
             Edite
           </button>
         )}
+        {isStatus ? <button onClick={cancelItem}>Cancel</button> : null}
       </div>
     </div>
   );
